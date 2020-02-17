f46c78b2a7d77de828d6aae4166037533721a7ea
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-17 13:58:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,8 +4,8 @@ class Dog
     @dog_name = dog_name
   end
   
-  def name=(dog_name)
-    @this_dogs_name = dog_name
+  def name=(this_dog_name)
+    @this_dog_name = dog_name
   end
   
   def name
